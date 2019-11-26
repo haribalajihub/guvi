9c2deed3727c94e6993117456f2aac74f76b5cf9
haribalajihub <54537748+haribalajihub@users.noreply.github.com>
2019-11-26 18:25:15 +0530

    Update E-bank.py

--- E-bank.py
+++ E-bank.py
@@ -1,21 +1,28 @@
-import random,time
+import random,time,sys
+def delete(acc):
+    del database[acc]
+    return 1
+try: color = sys.stdout.shell
+except AttributeError: raise RuntimeError("Use IDLE")
 database={}
+passdb={}
 c=0
 u=0
-print("---------------------WELCOME TO OUR E-BANK------------------------")
+color.write("WELCOME TO OUR H&D E-BANK".center(70,"-"),"COMMENT")
 time.sleep(1)
 print()
-print("--------------------by Hari Balaji et Dhina-----------------------")
+print("by Hari Balaji et Dhina".center(86,"-"))
 time.sleep(1)
 print()
 while True:     #HariBalaji module
-    SelectUser=input("Create Account(CA)/Admin Login(AL)/User Login(L) :").upper()                                       
+    SelectUser=input("Create Account(CA)/Admin Login(AL)/User Login(L) :").upper()
     if(SelectUser=="CA"):
         while True:
             import time
-            print("please wait your account number is generating")
+            color.write("please wait your account number is generating \n","KEYWORD")
             time.sleep(2)
             l=[]
+            op=[]
             x=random.randint(1000,2000)
             print("Your Account number is:",x)
             name=input("Your Name :")
@@ -24,9 +31,10 @@ while True:     #HariBalaji module
                 try:
                     bal=int(input("please deposit 5000Rs to activate your Account:"))
                     if(bal>=5000):
-                        print("Creating your account please wait")                                                    
+                        
+                        color.write("Creating your account...please wait.....\n","KEYWORD")
                         time.sleep(2)
-                        print("Your account created successfully")
+                        color.write("Your account created successfully@H&D E-bank \n","STRING")
                         time.sleep(1)
                         print("Your current balance :",bal)
 
@@ -42,15 +50,20 @@ while True:     #HariBalaji module
                         l.append(date)
                         l.append(time)
                         database[x]=l
+                        op.append(x)
+                        op.append("deposit  ")
+                        op.append(bal)
+                        op.append(bal)
+                        passdb[x]=op
                         c=1
                         break
                     else:
-                        print("Deposit should be more than 5000...")
+                        color.write("Deposit should be more than 5000...\n","COMMENT")
                         time.sleep(2)
                     if(c==1):
                         break
                 except ValueError:
-                    print("Deposit Should be numerical")
+                    color.write("Deposit Should be numerical....\n","COMMENT")
                 if(c==1):
                     break
             if(c==1):
@@ -60,14 +73,15 @@ while True:     #HariBalaji module
             import time
             try:
                 acc=int(input("Enter your Account number:"))
-                while True:
+                p=passdb[acc]
+                while True and bool(database)==True:
                     try:
                         username=input("Enter your username :")
                         passw=input("Enter your password :")
                         while True:
                             y=database[acc]
                             if(y[1]==username and y[2]==passw):
-                                choice=input("Enter your choice(Deposit(D),Withdraw(W),Balance(B) :").upper()       
+                                choice=input("Enter your choice(Deposit(D),Withdraw(W),Balance(B),Passbook(P) ,DeleteAccount(DA):").upper()       
                                 if(choice=="D"):
                                     try:
                                         deposit=int(input("How much do you like to deposit :"))
@@ -76,14 +90,19 @@ while True:     #HariBalaji module
                                         time.sleep(1)
                                         print(deposit,"rs credited in your Account...your balance is  :",y[3])
                                         time.sleep(1)
+                                        p.append("deposit  ")
+                                        p.append(deposit)
+                                        p.append(y[3])
+                                        passdb[acc]=p
                                         ce=input("Do you like to continue(C) or exit(E):").upper()
+                                        
                                         if(ce=="E"):
                                             c=0
                                             break
                                         else:
                                             continue
                                     except ValueError:
-                                        print("Deposit Should be numerical")
+                                        color.write("Deposit Should be numerical.....\n","COMMENT")
                                     if(c==0):
                                         break
                                 if(choice=="W"):
@@ -95,6 +114,10 @@ while True:     #HariBalaji module
                                         if(y[3]>=5000):
                                             print(withdraw,"rs debited in your Account...your balance is  :",y[3])
                                             time.sleep(1)
+                                            p.append("withdraw")
+                                            p.append(withdraw)
+                                            p.append(y[3])
+                                            passdb[acc]=p
                                             ce=input("Do you like to continue(C) or exit(E):").upper()
                                             if(ce=="E"):
                                                 c=0
@@ -102,18 +125,19 @@ while True:     #HariBalaji module
                                             else:
                                                 continue
                                         else:
-                                            print("minimum balance should be atleast 5000rs")
-                                            print("please try again")
+                                            color.write("Minimum balance should be atleast 5000rs.....\n","COMMENT")
+                                            print("Please try again....")
                                             y[3]=y[3]+withdraw
                                             continue
+                                        
                                     except ValueError:
-                                        print("Deposit Should be numerical")
+                                        color.write("Deposit Should be numerical....\n","COMMENT")
                                     if(c==0):
                                         break
                                 if(choice=="B"):
-                                    print("please wait...")
+                                    print("Please wait...")
                                     time.sleep(1)
-                                    print("your balance is :",y[3])
+                                    print("Your balance is :",y[3])
                                     time.sleep(1)
                                     ce=input("Do you like to continue(C) or exit(E) :").upper()
                                     if(ce=="E"):
@@ -121,10 +145,26 @@ while True:     #HariBalaji module
                                         break
                                     else:
                                         continue
+                                if(choice=="P"):
+                                    print("Please wait...")
+                                    ui=passdb[acc]
+                                    co=0
+                                    color.write("No\tTransaction\tC_or_D\t\tBalanace\n","COMMENT")
+                                    for i in range(1,len(ui),3):
+                                        co=co+1
+                                        print(co,"\t",ui[i],"\t",ui[i+1],"\t\t",ui[i+2])
+                                if(choice=="DA"):
+                                    time.sleep(1)
+                                    print("please wait...")
+                                    time.sleep(1)
+                                    delete(acc)
+                                    color.write("Your account has deleted........\n","COMMENT")
+                                    c=0
+                                    break
                                 if(c==0):
                                     break
                             else:
-                                print("invalid login")
+                                color.write("Invalid login....try again....\n","COMMENT")
                                 c=1
                                 break
                     
@@ -133,18 +173,20 @@ while True:     #HariBalaji module
                         if(c==0):
                             break
                     except KeyError:
-                        print("invalid account number")
-                        break
-                    if(c==0):
+                        color.write("Invalid account number....\n","COMMENT")
+                        c=1
+                    if(c==0 or c==1):
                         break
                 else:
-                    print("invalid login")
+                    color.write("Invalid login....\n","COMMENT")
                     c=1
                     break
                 if(c==0):
                     break
             except ValueError:
-                print("Enter valid Account number...")
+                color.write("Enter valid Account number...\n","COMMENT")
+            except KeyError:
+                color.write("Enter valid Account number...\n","COMMENT")
     elif(SelectUser=='AL'):         #HariBalaji module
         import time
         while True:
@@ -154,7 +196,8 @@ while True:     #HariBalaji module
                     if(ap=="admin" and bool(database)==True):
                         print("1.View all stakeholders")
                         print("2.view specific stakeholder")
-                        print("3.exit")
+                        print("3.delete specific stakeholder")
+                        print("4.exit")
                         checkuser=input("Choose option :")
                         if(checkuser=='1'):
                             cnt=1
@@ -163,19 +206,19 @@ while True:     #HariBalaji module
                                     print("Account Number      :",i[0])
                                     cnt=cnt+1
                                 if(cnt==2):
-                                    print("Name                :",i[1])
+                                    print("Name                       :",i[1])
                                     cnt=cnt+1
                                 if(cnt==3):
-                                    print("Password            :",i[2])
+                                    print("Password                 :",i[2])
                                     cnt=cnt+1
                                 if(cnt==4):
-                                    print("Balance             :",i[3])
+                                    print("Balance                   :",i[3])
                                     cnt=cnt+1
                                 if(cnt==5):
                                     print("Creation of Account :",i[4],i[5])                        
                                     cnt=1
-                                
-                                print("-------------------------------------------------")
+                                    break
+                                color.write("-------------------------------------------------\n","KEYWORD")
                                 time.sleep(0.5)
                             ce=input("Do you like to continue(C) or exit(E):").upper()
                             if(ce=="E"):
@@ -192,26 +235,26 @@ while True:     #HariBalaji module
                                     cnt=1
                                     for kl in lis:
                                         if(cnt==1):
-                                            print("Account Number      :",kl)
+                                            print("Account Number       :",kl)
                                             cnt=cnt+1
                                             continue
                                         if(cnt==2):
-                                            print("Name                :",kl)
+                                            print("Name                       :",kl)
                                             cnt=cnt+1
                                             continue
                                         if(cnt==3):
-                                            print("Password            :",kl)
+                                            print("Password                 :",kl)
                                             cnt=cnt+1
                                             continue
                                         if(cnt==4):
-                                            print("Balance             :",kl)
+                                            print("Balance                   :",kl)
                                             cnt=cnt+1
                                             continue
                                         if(cnt==5):
                                             print("Creation of Account :",i[4],i[5])
                                             cnt=1
                                             break
-                                    print("-------------------------------------------------")
+                                    color.write("-------------------------------------------------\n","KEYWORD")
                                     time.sleep(1)
                                     ce=input("Do you like to continue(C) or exit(E):").upper()
                                     if(ce=="E"):
@@ -221,10 +264,10 @@ while True:     #HariBalaji module
                                         c=1
                                         break
                                 except KeyError:
-                                    print("invalid account number")
+                                    color.write("invalid account number....\n","COMMENT")
                                     c=1
                                 except ValueError:
-                                    print("invalid account number")
+                                    color.write("invalid account number....\n","COMMENT")
                                     c=1
                                     
                                 if(c==0):
@@ -236,23 +279,32 @@ while True:     #HariBalaji module
                             if(c==1):
                                 continue
                         elif(checkuser=='3'):
+                            delspecific=int(input("Enter Account Number to remove account from H&D E-bank :"))
+                            delete(delspecific)
+                            time.sleep(1)
+                            print("please wait...")
+                            time.sleep(1)
+                            color.write("Account has deleted........\n","COMMENT")
+                            c=0
+                            break
+                        elif(checkuser=='4'):
                             u=0
                             break
                         else:
-                            print("Please choose the given field...")
+                            color.write("Please choose the given field...\n","COMMENT")
                     if(c==1):
                         continue
                     else:
-                        print("no stakeholder currently ....redirect to home")
+                        color.write("no stakeholder currently....redirect to home....\n","COMMENT")
                         time.sleep(2)
                     if(u==0):
                         break
                 if(u==0):
                     break
             else:
-                print("invalid password")
+                color.write("Invalid password....\n","COMMENT")
                 continue
                     
     else:
-        print("please choose the given criteria....")
+        color.write("Please choose the given criteria....\n","COMMENT")
         
